45a17d618d5f0403aa55e2c5b36dd7f59b1e0973
Vanessa Chalem <vchalem@salesforce.com>
2019-03-21 12:43:57 -0400

    adding LWC

---
+++ force-app/main/default/lwc/trialinfodisplay/trialinfodisplay.js
@@ -0,0 +1,39 @@
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+
+const FIELDS = [
+    'Contact.Name',
+    'Contact.Email',
+    'Contact.Title',
+    'Contact.HealthCloudGA__Gender__c',
+    'Contact.HealthCloudGA__Age__c',
+    //'Contact.CleanStatus',
+    //'Contact.MailingAddress'
+
+];
+
+
+export default class trialinfodisplay extends LightningElement {
+    @api recordId;
+
+    @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
+    contact;
+
+    get name() {
+        return this.contact.data.fields.Name.value;
+    }
+    get email() {
+        return this.contact.data.fields.Email.value;
+    }
+    get title() {
+        return this.contact.data.fields.Title.value;
+    }
+    get gender() {
+        return this.contact.data.fields.HealthCloudGA__Gender__c.value;
+    }
+    get age(){
+        return this.contact.data.fields.HealthCloudGA__Age__c.value;
+    } 
+
+
+}
