7a5e50f8be8d2d524091c5cfa598c1f1f061e9d4
Vanessa Chalem <vchalem@salesforce.com>
2019-03-22 01:04:21 -0400

    apx to pass NCId

---
+++ .sfdx/typings/lwc/apex/CreateTestData.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/CreateTestData.getTestData" {
+  export default function getTestData(): Promise<any>;
+}
--- force-app/main/default/lwc/trialinfodisplay/trialinfodisplay.js
+++ force-app/main/default/lwc/trialinfodisplay/trialinfodisplay.js
@@ -1,30 +1,49 @@
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, track, api, wire } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
+import getTestData from '@salesforce/apex/CreateTestData.getTestData';
 
 const FIELDS = [
     'Contact.Name',
     'Contact.Email',
     'Contact.HealthCloudGA__Gender__c',
     'Contact.HealthCloudGA__Age__c',
-    'Contact.MailingAddress' //state
+   // 'Contact.MailingAddress' //state
 
 ];
 
 
 export default class trialinfodisplay extends LightningElement {
     @api recordId;
+    @track NCI;
 
     @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
     contact;
 
-    //send recordID to callout 
+    @wire(getTestData)
+    idReturned({ error, data }) {
+        if (data){
+            this.NCI = data;
+            this.error=undefined;
+        }else if (error){
+            this.error=error;
+            this.NCI=undefined;
+        }
+    }      
+    
+    
+    get url(){
+       // var id= "NCI-2017-01240";
+        var url = "https://www.cancer.gov/about-cancer/treatment/clinical-trials/search/v?id=";
+        var link = url.concat(this.NCI);
+        return link;
+    }
+
+    handleLoad(){
+        //will be an action unto a flow screen...
+    }
+
+    //send contact recordID to callout 
     //do call to apx class - 
     //return to apx class the test data to display
-
-//url concatenate
-//https://www.cancer.gov/about-cancer/treatment/clinical-trials/search/v?id=  NCI-2017-01240
-
-
-
 
 }
