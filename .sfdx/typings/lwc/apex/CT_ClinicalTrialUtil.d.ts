a4d0c8bf8f5f71d32c1e70702ca15d57757b17bf
Vanessa Chalem <vchalem@salesforce.com>
2019-03-25 17:40:03 -0400

    cacheable

---
+++ .sfdx/typings/lwc/apex/CT_ClinicalTrialUtil.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/CT_ClinicalTrialUtil.getTrialsForContact" {
+  export default function getTrialsForContact(param: {recordId: any}): Promise<any>;
+}
--- force-app/main/default/lwc/trialinfodisplay/trialinfodisplay.js
+++ force-app/main/default/lwc/trialinfodisplay/trialinfodisplay.js
@@ -1,57 +1,56 @@
 import { LightningElement, track, api, wire } from 'lwc';
-import { getRecord } from 'lightning/uiRecordApi';
-import getTestData from '@salesforce/apex/CreateTestData.getTestData';
+//import getTestData from '@salesforce/apex/CreateTestData.getTestData';
+import getTrialsForContact from '@salesforce/apex/CT_ClinicalTrialUtil.getTrialsForContact';
 
-const FIELDS = [
-    //contact fields that are being queried
-    'Contact.Id',
-    'Contact.Name',
-    'Contact.Email',
-    'Contact.HealthCloudGA__Gender__c',
-    'Contact.HealthCloudGA__Age__c',
-   // 'Contact.MailingAddress' //state
-
-];
 
 
 export default class trialinfodisplay extends LightningElement {
     @api recordId;
     @track NCI;
     @track recId = ' ';
- 
-    @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
-    contact;
-  
-    //recId = this.contact.data.fields.Id.value;
- 
-    get id(){
-        return this.contact.data.fields.Id.value;
-    }
 
-/** 
+ 
     //when LWC loads - call apx class w/ contact ID
-    @wire(getTestData, {recordId: '$recId'})
-    idReturned({ error, data }) {
-        if (data){
+    @wire(getTrialsForContact, {recordId: '$recordId'})
+    wiredContact({ error, data }) {
+        if (data) {
             this.NCI = data;
-            this.error=undefined;
-        }else if (error){
-            this.error=error;
-            this.NCI=undefined;
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.NCI = undefined;
         }
-    }      
-
-    */
-    
-    
-    get url(){
-        var url = "https://www.cancer.gov/about-cancer/treatment/clinical-trials/search/v?id=";
-        var link = url.concat(this.NCI);
-        return link;
     }
 
-    handleLoad(){
-        //will be an action unto a flow screen...
+    get stringJson(){
+        return this.wiredContact.data;
     }
-
 }
+
+
+    
+    //idReturned({ error, data }) {
+
+
+        // if (data){
+          //  this.NCI = data;
+           // this.error=undefined;
+        //}else if (error){
+          //  this.error=error;
+          //  this.NCI=undefined;
+        //}
+   // }      
+
+    
+    
+    
+    //get url(){
+      //  var url = "https://www.cancer.gov/about-cancer/treatment/clinical-trials/search/v?id=";
+       // var link = url.concat(this.NCI);
+        //return link;
+   // }
+
+    //handleLoad(){
+        //will be an action unto a flow screen...
+    //}
+
